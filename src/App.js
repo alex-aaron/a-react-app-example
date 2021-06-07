232f08ba0e54f7dd6e1a9969a23e55a256effb1d
Alex <alexfrancisaaron@gmail.com>
2021-06-07 15:12:06 -0500

    added button component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import moment from 'moment';
-import ExampleComponent from './ExampleComponent'
+import ButtonComponent from './ButtonComponent'
 import TestComponent from './TestComponent'
 
 // Add your code own within the return statement
@@ -10,17 +9,8 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          Now
-        </header>
-        <p className="App-intro">
-          In React apps, we write JSX - it looks like HTML, and uses a lot of HTML syntax.
-          JSX lets us include JavaScript functions right along with the HTML, and also
-          allows us to add in components, which are separate, self-contained chunks of JSX.
-        </p>
-        <ExampleComponent />
-        <TestComponent />
-
+        <h1>Test React App</h1>
+        <ButtonComponent />
       </div>
     );
   }
---
+++ src/ButtonComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// no need to modify anything in here. The second test is looking for the returned JSX from this component
+class ButtonComponent extends Component {
+  render() {
+    return (
+      <div>
+        <button>Select</button>
+      </div>
+    );
+  }
+}
+
+export default ButtonComponent;
