d41711d2f4dcc7da9d60ebf56dc633f516bd11fc
Alex <alexfrancisaaron@gmail.com>
2021-08-12 15:18:00 -0500

    revised TestComponent to functional component

--- src/TestComponent.js
+++ src/TestComponent.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
 // no need to modify anything in here. The final test is looking for the returned JSX from this component
-class TestComponent extends Component {
-  render() {
-    return (
-      <div className="video">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/5TbUxGZtwGI" frameBorder="0" allow="autoplay; encrypted-media" title="time video" allowFullScreen></iframe>
-      </div>
-    );
-  }
+const TestComponent = props => {
+  return (
+    <h2>Test Compoment</h2>
+  )
 }
 
 export default TestComponent;
