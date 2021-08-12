b7713af6addbbcea6efb8564b493aac8e29dd1ea
Alex <alexfrancisaaron@gmail.com>
2021-08-12 15:16:27 -0500

    added test functional component to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ButtonComponent from './ButtonComponent'
 import TestComponent from './TestComponent'
+import TestFunctionalComponent from './TestFunctionalComponent'
 
 // Add your code own within the return statement
 
@@ -10,6 +11,8 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Test React App</h1>
+        <TestComponent />
+        <TestFunctionalComponent />
         <ButtonComponent />
       </div>
     );
--- src/TestFunctionalComponent.js
+++ src/TestFunctionalComponent.js
@@ -1,3 +1,6 @@
+import React from 'react';
+
+
 const TestFunctionalComponent = () => {
     return (
         <h2>Test Functional component</h2>
